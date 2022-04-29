f44d378bcdadda2ddf1c2de8e1a87b1c84a20ab9
joshphuocdang <101201357+joshphuocdang@users.noreply.github.com>
2022-04-29 22:20:44 +0700

    Update ifttt.ino

--- ifttt.ino
+++ ifttt.ino
@@ -1,3 +1,5 @@
+// This #include statement was automatically added by the Particle IDE.
+#include <thingspeak-webhooks.h>
 
 
 int light = A4;
@@ -9,13 +11,30 @@ void setup()
 
 void loop()
 {
-  int lightLevel = digitalRead(light);
+  int lightLevel = analogRead(light);
   
-  if (lightLevel < 10) Particle.publish("lightLevel", "dark", PRIVATE);
-  if (lightLevel < 200) Particle.publish("lightLevel", "dim", PRIVATE);
-  if (lightLevel <500) Particle.publish("lightLevel", "light", PRIVATE);
-  if(lightLevel < 800) Particle.publish("lightLevel", "bright",PRIVATE);
-  else Particle.publish("lightLevel", "very bright", PRIVATE);
-  delay(50000);
-
+  if (lightLevel < 10) 
+  {
+      Particle.publish("lightLevel", "dark", PRIVATE);
+  }
+  else if (lightLevel < 200 && lightLevel >= 10)
+  {
+      Particle.publish("lightLevel", "dim", PRIVATE);
+  }
+  
+  else if (lightLevel <500 && lightLevel >=200)
+  {
+      Particle.publish("lightLevel", "light", PRIVATE);
+  }
+  
+  else if(lightLevel < 800 && lightLevel >= 500) 
+  {
+      Particle.publish("lightLevel", "bright",PRIVATE);
+  }
+  else if (lightLevel > 800)
+  {
+      Particle.publish("lightLevel", "very bright", PRIVATE);
+  }
+  
+  delay(5000);
 }
